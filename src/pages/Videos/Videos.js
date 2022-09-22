1b853203467605c277ae89285d4126b821b9710d
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-22 15:34:16 -0700

    begin adding the three pages for the site

---
+++ src/pages/Videos/Videos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Videos = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Videos;
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from './Home/Home';
+export { default as Videos } from './Videos/Videos';
+export { default as Research } from './Research/Research';
