ab54126e3bac7e2d4fd77b30d9a00da3669c7095
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-22 15:33:58 -0700

    add paths for three pages and topnav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Home, Videos, Research } from "./pages";
+import { TopNav } from "./components";
 
 function App() {
   return (
     <>
-   
+      <Router>
+        <TopNav />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/videos' exact element={<Videos />} />
+          <Route path='/research' exact element={<Research />} />
+        </Routes>
+      </Router>
     </>
   );
 }
