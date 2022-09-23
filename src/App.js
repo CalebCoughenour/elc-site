467063de59b8b783f0ec622c15f75d35cf07b17d
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-23 14:43:35 -0700

    add footer to router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Home, Videos, Research } from "./pages";
-import { TopNav } from "./components";
+import { TopNav, Footer } from "./components";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
           <Route path='/videos' exact element={<Videos />} />
           <Route path='/research' exact element={<Research />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
