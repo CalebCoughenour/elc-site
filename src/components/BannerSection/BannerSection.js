fede3bb5f1d53eb80caaddafe9432406c865aa39
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-23 14:42:46 -0700

    begin work on the banner for homepage

---
+++ src/components/BannerSection/BannerSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BannerSec } from './BannerSection.elements';
+
+
+function BannerSection() {
+  return (
+    <BannerSec>
+
+    </BannerSec>
+  );
+}
+
+export default BannerSection;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { BannerSection } from '../../components';
+
 
 const Home = () => {
   return (
     <>
-      
+      <BannerSection />
     </>
   )
 }
