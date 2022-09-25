cd1f669ffaf21d7a56049ed3087dd9ccd923bda1
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-24 23:02:17 -0700

    begin converting components in a reusuable component

--- src/components/MissionSection/MissionSection.elements.js
+++ src/components/MissionSection/MissionSection.elements.js
@@ -1,145 +1,45 @@
 import styled from "styled-components";
 
 export const MissionSec = styled.div`
-  padding: 500px 0px;
+  padding: 100px 0px;
   background: #383232;
 `;
 
-export const MissionTitle = styled.h1`
-  position: relative;
-  overflow: hidden; 
-  font-size: 50px;
-  margin-left: 30px;
-  color: #DCDCDE;
+// export const MissionTitle = styled.h1`
+//   position: relative;
+//   overflow: hidden; 
+//   font-size: 50px;
+//   margin-left: 30px;
+//   color: #DCDCDE;
 
-  &:after {
-    display: inline-block;
-    content: "";
-    height: 2px;
-    background: #DCDCDE;
-    position: absolute;
-    width: 80%;
-    top: 50%;
-    margin-top: -2px;
-    margin-left: 10px;
-  }
+//   &:after {
+//     display: inline-block;
+//     content: "";
+//     height: 2px;
+//     background: #DCDCDE;
+//     position: absolute;
+//     width: 80%;
+//     top: 50%;
+//     margin-top: -2px;
+//     margin-left: 10px;
+//   }
+// `;
+
+export const MissionTextSection = styled.div`
+  padding: 100px 0px;
 `;
 
 export const MissionText = styled.h1`
-  color: #DCDCDE;
-  font-size: 50px;
-  font-weight: bold;
-`;
-
-export const MissionSecVideo = styled.video`
-margin-top: 10px;
-margin-right: 20px;
-float: right;
-background: #dcdcdc;
-width: 50%;
-height: 50%;
-`;
-
-export const MissionTextSec = styled.div`
-  padding: 100px 0px;
-`;
-
-export const AboutSec = styled.div`
-  padding: 100px 0;
-  background-color: #dcdcdc;
-`;
-
-export const Title = styled.h1`
-  font-family: Brush Script MT;
-  font-size: 50px;
-  text-align: center;
-`
-
-export const BannerText = styled.h1`
-  margin: auto; 
-  padding: 12px 10px;
-  background-color: rgba(0, 0, 0, .9);
-  color: white;
-  width: 40%;
-  height: 20%;
-  flex-wrap: wrap;
-  flex-direction: column;
-  @media screen and (max-width: 993px) {
-    height: 30%;
-  }
-  @media screen and (max-width: 768px) {
-    height: 50%;
-  }
-  @media screen and (max-width: 600px) {
-    height: 60%;
-  }
-  @media screen and (max-width: 500px) {
-    width: 60%;
-    height: 70%
-  }
-  @media screen and (max-width: 450px) {
-    margin: 30px 10px;
-  }
-  @media screen and (max-width: 350px) {
-    font-size: 20px;
-  }
-  @media screen and (max-width: 300px) {
-    font-size: 15px;
-  }
-`;
-
-export const BannerTextWrapper = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  color: #fff;
-  @media screen and (max-width: 770px) {
-    height: 80%;
-  }
-  @media screen and (max-width: 600px) {
-    height: 70%;
-  }
-`;
-
-export const AboutTextSection = styled.div`
-  padding: 100px 0px;
-`;
-
-export const AboutTextTitle = styled.h1`
-  text-align: center;
-  text-decoration: underline;
-  @media screen and (max-width: 400px) {
-    font-size: 20px;
-  }
-`;
-
-export const Img = styled.img`
-  margin-right: 20px;
-  float: left;
-  @media screen and (max-width: 770px) {
-    width: 50%;
-  }
-  @media screen and (max-width: 600px) {
-    width: 40%;
-  }
-`;
-
-export const AboutParagraph = styled.h1`
   margin-left: 5px;
   font-size: 60px;
   text-align: left;
+  color: #DCDCDE;
   @media screen and (max-width: 770px) {
     font-size: 20px;
   }
 `;
 
-export const Video = styled.video`
+export const Media = styled.video`
   margin-top: 10px;
   margin-right: 20px;
   float: left;
--- src/components/MissionSection/MissionSection.js
+++ src/components/MissionSection/MissionSection.js
@@ -1,48 +1,29 @@
 import React from "react";
-import { MissionSec, MissionTitle, MissionText, MissionSecVideo, MissionTextSec } from "./MissionSection.elements";
-import { AboutSec, BannerTextWrapper, BannerText, AboutTextSection, AboutTextTitle, Img, AboutParagraph, Video, VideoSection } from './MissionSection.elements';
+import { MissionSec, MissionText, MissionTextSection, Media } from "./MissionSection.elements";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-const MissionSection = () => {
+
+const MissionSection = ({ sectionText, media }) => {
   return(
     <>
-      {/* <MissionSec>
-        <Container>
-          <MissionTitle>Our Mission</MissionTitle>
-          <Row>
-            <Col md={true}>
-              <MissionTextSec>
-                <Container>
-                  <MissionText>We believe global adoption of crypto and web3 is right around the corner.</MissionText>
-                  <MissionSecVideo src="https://assets.mixkit.co/videos/preview/mixkit-world-map-in-a-digital-world-12748-large.mp4" autoPlay muted loop />
-                </Container>
-              </MissionTextSec>
-            </Col>
-          </Row>
-
-          
-        </Container>
-      </MissionSec> */}
-
-
-``````<AboutSec>
-        <Container>
-          <Row>
-            <Col md={true}>
-              <AboutTextSection>
-                <Container>                 
-                  <Video src='https://assets.mixkit.co/videos/preview/mixkit-world-map-in-a-digital-world-12748-large.mp4' autoPlay muted loop />
-                  <AboutParagraph>
-                    We believe global adoption of crypto and web3 is right around the corner.
-                  </AboutParagraph>
-                </Container>
-              </AboutTextSection>
-            </Col>
-          </Row>
-        </Container>
-      </AboutSec>
+        <MissionSec>          
+            <Container>
+              <Row>
+                <Col md={true}>
+                  <MissionTextSection>
+                    <Container>                 
+                      <Media src={media} autoPlay muted loop />
+                      <MissionText>
+                        {sectionText}
+                      </MissionText>
+                    </Container>
+                  </MissionTextSection>
+                </Col>
+              </Row>
+            </Container>
+      </MissionSec>
     </>
   )
 };
