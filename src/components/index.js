e0e1b5fc97d3b37652a3ab03c244661153fb48b0
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-25 19:51:44 -0700

    add export for right mission section

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as TopNav } from './TopNav/TopNav';
 export { default as Footer } from './Footer/Footer';
 export { default as BannerSection } from './BannerSection/BannerSection';
 export { default as MissionSection } from './MissionSection/MissionSection';
+export { default as MissionSectionRight } from './MissionSectionRight/MissionSectionRight';
