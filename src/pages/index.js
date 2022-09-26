ab095066c111fd27361a34bc11cfefd28f778d48
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-25 19:43:42 -0700

    add export for news page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './Home/Home';
+export { default as News } from './News/News';
 export { default as Videos } from './Videos/Videos';
 export { default as Research } from './Research/Research';
