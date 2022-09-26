80c2d5caa520407c71d931b53c2830a1b1ce5534
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-25 19:39:29 -0700

    add route for news page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { Home, Videos, Research } from "./pages";
+import { Home, Videos, Research, News } from "./pages";
 import { TopNav, Footer } from "./components";
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -13,6 +13,7 @@ function App() {
           <Route path='/' exact element={<Home />} />
           <Route path='/videos' exact element={<Videos />} />
           <Route path='/research' exact element={<Research />} />
+          <Route path='/news' exact element={<News />} />
         </Routes>
         <Footer />
       </Router>
