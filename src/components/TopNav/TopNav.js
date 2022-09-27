46ad0785e3b69047acba923ac58e80c971c382b0
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-26 21:20:43 -0700

    update top nav with bootstrap navbar

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,37 +1,31 @@
-import React from 'react';
-import { TopNavSection, SocialIconLink, TopNavLink } from './TopNav.elements';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import { FaTwitter, FaYoutube } from 'react-icons/fa';
-
+import { SocialIconLink} from './TopNav.elements';
 
 function TopNav() {
   return (
-    <TopNavSection>
-      <Container style={{ background: '#1D1A1A'}}>
-        <Row style={{ background: '#1D1A1A', textAlign: 'center'}}>
-
-          <Col>
-            {/* Nav links */}
-            <TopNavLink href='/'>Home</TopNavLink>
-            <TopNavLink href='/videos'>Videos</TopNavLink>
-            <TopNavLink href='/research'>Research</TopNavLink>
-            <TopNavLink href='/news'>News</TopNavLink>
-
-            {/* Social Links */}
+    <Navbar expand="lg" style={{ background: '#1D1A1A', textAlign: 'center' }} >
+      <Container>
+        <Navbar.Brand href="/" style={{ color: '#ffffff' }}>Everyone Learns Crypto</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ color: '#ffffff' }}/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/" style={{ color: '#ffffff' }}>Home</Nav.Link>
+            <Nav.Link href="/videos" style={{ color: '#ffffff' }}>Videos</Nav.Link>
+            <Nav.Link href="/research" style={{ color: '#ffffff' }}>Research</Nav.Link>
+            <Nav.Link href="/News" style={{ color: '#ffffff' }}>News</Nav.Link>
             <SocialIconLink href={'//www.twitter.com/belaccoke'} target="_blank" rel="noopener noreferrer" aria-label="Twitter">
               <FaTwitter style={{ background: '#1D1A1A'}} />
             </SocialIconLink>
             <SocialIconLink href={'//www.youtube.com/channel/UCMsqHdA66Awh_ljOYV8iKkQ'} target="_blank" rel="noopener noreferrer" aria-label="Youtube">
               <FaYoutube style={{ background: '#1D1A1A'}} />
             </SocialIconLink>
-          </Col>
-
-
-        </Row>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
-    </TopNavSection>
+    </Navbar>
   );
 }
 
