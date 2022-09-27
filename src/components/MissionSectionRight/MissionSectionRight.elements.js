b0191b2cd0d31956f3ce1b34388dc3a617a52611
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-26 22:27:20 -0700

    update styling for media component

--- src/components/MissionSectionRight/MissionSectionRight.elements.js
+++ src/components/MissionSectionRight/MissionSectionRight.elements.js
@@ -2,17 +2,23 @@ import styled from "styled-components";
 
 export const MissionSec = styled.div`
   background: #383232;
+  padding: 50px 0px;
+
+  @media screen and (max-width: 400px) {
+    padding: 0px 0px;
+  }
 `;
 
 export const MissionTextSection = styled.div`
   padding: 100px 0px;
+
+  @media screen and (max-width: 600px) {
+    padding: 0px 0px;
+  }
 `;
 
 export const MissionText = styled.h1`
   margin-right: 20px;
-  position: absolute;
-  top: 50%;
-  right: 50%;
   font-size: 55px;
   text-align: left;
   color: #DCDCDE;
@@ -32,14 +38,28 @@ export const MissionText = styled.h1`
   @media screen and (max-width: 400px) {
     font-size: 20px;
   }
+
+  @media screen and (min-width: 600px) {
+    position: absolute;
+    top: 50%;
+    right: 50%;
+  }
 `;
 
 export const Media = styled.img`
   margin-top: 10px;
   margin-right: 20px;
   margin-left: 20px;
-  float: right;
-  background: #dcdcdc;
-  width: 50%;
-  height: 50%;
+  background: #383232;
+
+  @media screen and (min-width: 600px) {
+    float: right;
+    width: 50%;
+   height: 50%; 
+  }
+
+  @media screen and (max-width: 600px) {
+   width: 70%;
+   height: 70%; 
+  }
 `;
