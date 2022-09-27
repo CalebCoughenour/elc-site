fd6f785b47062d038e9b4fd791eb4ac4fddf7f74
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-26 21:19:35 -0700

    add alt tags and update images/videos

--- src/pages/Home/Data.js
+++ src/pages/Home/Data.js
@@ -1,19 +1,23 @@
 export const missionObjOne = {
-  sectionText: "World wide adoption of cryptocurrency is inevitable and fast approaching",
-  media: "images/homepage/world-map.mp4"
+  sectionText: "World wide adoption of cryptocurrency is inevitable and fast approaching.",
+  media: "images/homepage/world-map.mp4",
+  alt: 'A video looping the world map with tech designs scrolling'
 };
 
 export const missionObjTwo = {
-  sectionText: "We believe onboarding and education is essential to the process",
-  media: "images/homepage/btc-circling.mp4"
+  sectionText: "There is a major learning curve for newcomers, and our mission is to help.",
+  media: "images/homepage/thinking-person.webp",
+  alt: 'A person thinking and sitting at a desk with a laptop and two computer screens'
 };
 
 export const missionObjThree = {
-  sectionText: "We believe global adoption of crypto and web3 is right around the corner",
-  media: "images/homepage/world-map.mp4"
+  sectionText: "We believe onboarding and education are essential to the process.",
+  media: "images/homepage/tech-devices.mp4",
+  alt: 'A cartoon video looping different technical devices and charts'
 };
 
 export const missionObjFour = {
-  sectionText: "We believe global adoption of crypto and web3 is right around the corner",
-  media: "images/homepage/btc-circling.mp4"
+  sectionText: "Our goal is to provide step-by-step walkthroughs to help the least savvy user.",
+  media: "images/homepage/thinking-person.webp",
+  alt: 'A person thinking and sitting at a desk with a laptop and two computer screens'
 };
