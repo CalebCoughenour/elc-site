88cabec2d92849db1db79f5424bc9ca25bbd566f
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-26 22:40:13 -0700

    update data file

--- src/pages/Home/Data.js
+++ src/pages/Home/Data.js
@@ -1,23 +1,23 @@
 export const missionObjOne = {
-  sectionText: "World wide adoption of cryptocurrency is inevitable and fast approaching.",
+  sectionText: "World wide adoption of cryptocurrency is inevitable and fast approaching",
   media: "images/homepage/world-map.mp4",
   alt: 'A video looping the world map with tech designs scrolling'
 };
 
 export const missionObjTwo = {
-  sectionText: "There is a major learning curve for newcomers, and our mission is to help.",
+  sectionText: "There is a learning curve for all new users, and our mission is to help them succeed",
   media: "images/homepage/thinking-person.webp",
   alt: 'A person thinking and sitting at a desk with a laptop and two computer screens'
 };
 
 export const missionObjThree = {
-  sectionText: "We believe onboarding and education are essential to the process.",
+  sectionText: "We believe onboarding and education are essential to the process",
   media: "images/homepage/tech-devices.mp4",
   alt: 'A cartoon video looping different technical devices and charts'
 };
 
 export const missionObjFour = {
-  sectionText: "Our goal is to provide step-by-step walkthroughs to help guide new users.",
+  sectionText: "ELC provides easy to understand walkthroughs and guides to help new users learn",
   media: "images/homepage/thinking-person.webp",
   alt: 'A person thinking and sitting at a desk with a laptop and two computer screens'
 };
