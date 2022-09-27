c55599f8548ba8614ece1b0513fdce5b7934ff33
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-26 22:28:56 -0700

    update media and text section for mobile

--- src/components/MissionSection/MissionSection.elements.js
+++ src/components/MissionSection/MissionSection.elements.js
@@ -2,37 +2,23 @@ import styled from "styled-components";
 
 export const MissionSec = styled.div`
   background: #383232;
+  padding: 50px 0px;
+
+  @media screen and (max-width: 400px) {
+    padding: 0px 0px;
+  }
 `;
-
-// export const MissionTitle = styled.h1`
-//   position: relative;
-//   overflow: hidden; 
-//   font-size: 50px;
-//   margin-left: 30px;
-//   color: #DCDCDE;
-
-//   &:after {
-//     display: inline-block;
-//     content: "";
-//     height: 2px;
-//     background: #DCDCDE;
-//     position: absolute;
-//     width: 80%;
-//     top: 50%;
-//     margin-top: -2px;
-//     margin-left: 10px;
-//   }
-// `;
 
 export const MissionTextSection = styled.div`
   padding: 100px 0px;
+
+  @media screen and (max-width: 600px) {
+    padding: 0px 0px;
+  }
 `;
 
 export const MissionText = styled.h1`
   margin: 0;
-  position: absolute;
-  top: 50%;
-  left: 50%;
   font-size: 55px;
   text-align: center;
   color: #DCDCDE;
@@ -57,13 +43,27 @@ export const MissionText = styled.h1`
   @media screen and (max-width: 400px) {
     font-size: 20px;
   }
+
+  @media screen and (min-width: 600px) {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+  }
 `;
 
 export const Media = styled.video`
   margin-top: 10px;
   margin-right: 20px;
-  float: left;
-  background: #dcdcdc;
-  width: 50%;
-  height: 50%;
+  background: #383232;
+
+  @media screen and (min-width: 600px) {
+    float: left;
+    width: 50%;
+    height: 50%; 
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 70%;
+    height: 70%; 
+  }
 `;
