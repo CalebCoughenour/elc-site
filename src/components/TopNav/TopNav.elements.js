8d3f10bf13dc868f156bae95315ba13dc854e33f
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-26 21:21:03 -0700

    removed unused styled components

--- src/components/TopNav/TopNav.elements.js
+++ src/components/TopNav/TopNav.elements.js
@@ -1,25 +1,5 @@
 import styled from "styled-components";
 
-export const TopNavSection = styled.div`
-  width: 100%;
-  padding: 10px 0px;
-  background: #1D1A1A;
-`;
-
-export const TopNavLink = styled.a`
-  text-decoration: none;
-  color: #DCDCDE;
-  cursor: pointer;
-  margin-left: 20px;
-  font-size: 20px;
-  background: #1D1A1A;
-
-  &:hover {
-    color: #A4A4AA;
-    transition: 0.2s ease-out;
-  }
-`;
-
 export const SocialIconLink = styled.a`
   margin-left: 20px;
   color: #DCDCDE;
