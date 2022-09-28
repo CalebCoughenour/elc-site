1558a2c0c8ec3264b63c202c70aafe193e373a73
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-27 21:33:06 -0700

    add export for home video section

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from './Footer/Footer';
 export { default as BannerSection } from './BannerSection/BannerSection';
 export { default as MissionSection } from './MissionSection/MissionSection';
 export { default as MissionSectionRight } from './MissionSectionRight/MissionSectionRight';
+export { default as HomeVideoSection } from './HomeVideoSection/HomeVideoSection';
