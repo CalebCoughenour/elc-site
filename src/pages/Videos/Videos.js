de1f782c09b5f4455832d888d610dad34c207006
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-29 22:23:22 -0700

    add banner section to top of page with test components below

--- src/pages/Videos/Videos.js
+++ src/pages/Videos/Videos.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BannerSection } from '../../components';
+import { bannerObj } from './Data';
+import TestComp from '../../components/TestComponent';
 
 const Videos = () => {
   return (
     <>
-      
+      <BannerSection {...bannerObj} />
+      <div style={{ background: '#383232' }} >
+        <TestComp />
+        <TestComp />
+        <TestComp />
+      </div>
     </>
   )
 }
