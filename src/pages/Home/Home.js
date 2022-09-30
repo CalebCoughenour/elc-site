43c1266d36d8e0d7dfddbf868dec5f989d0b3cc1
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-29 22:25:04 -0700

    add banner object to banner section component'

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import  { missionObjOne, missionObjTwo, missionObjThree, missionObjFour } from './Data';
+import  { missionObjOne, missionObjTwo, missionObjThree, missionObjFour, bannerObj } from './Data';
 import { BannerSection, MissionSection, MissionSectionRight, HomeVideoSection } from '../../components';
 
 
 const Home = () => {
   return (
     <>
-      <BannerSection />
+      <BannerSection {...bannerObj} />
       <div style={{ background: '#383232' }} >
         <MissionSection {...missionObjOne} />
         <MissionSectionRight {...missionObjTwo} />
