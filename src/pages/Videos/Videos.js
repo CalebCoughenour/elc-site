7fd8208663ad4f8b99dbc258276505428ec230a9
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-30 18:27:03 -0700

    add video card section component

--- src/pages/Videos/Videos.js
+++ src/pages/Videos/Videos.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BannerSection } from '../../components';
 import { bannerObj } from './Data';
-import TestComp from '../../components/TestComponent';
+import { VideoCardSection } from '../../components';
 
 const Videos = () => {
   return (
     <>
       <BannerSection {...bannerObj} />
       <div style={{ background: '#383232' }} >
-        <TestComp />
-        <TestComp />
-        <TestComp />
+        <VideoCardSection />  
       </div>
     </>
   )
