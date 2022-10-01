2f044018feb1e0dc25031cc34170d67a8c5974bc
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-30 18:30:10 -0700

    add component that takes in props for video card

---
+++ src/components/VideoCard/VideoCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import { CardOverlay, CardTitle, CardText, CardButton, YTVideo } from './VideoCard.elements';
+import './VideoCard.css';
+
+const VideoCard = (props) => {
+  return (
+    <>            
+      <Card className='video-card' style={{ border: '1px solid black'}}>
+        <CardOverlay className='card-overlay'/>
+        <YTVideo className='video-card-video' src={props.cardSrc} alt={props.cardAlt} />
+        <CardText>
+          <CardTitle>{props.cardTitle}</CardTitle>
+          <Link to={props.linkPath}>
+            <CardButton className='card-link-button'>
+              BROWSE
+            </CardButton>
+          </Link>
+        </CardText>
+      </Card>
+    </>
+  )
+};
+
+VideoCard.propTypes = {
+  cardImg: PropTypes.string,
+  cardImgAlt: PropTypes.string,
+  cardTitle: PropTypes.string,
+  linkPath: PropTypes.string
+};
+
+export default VideoCard;
