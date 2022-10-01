455c4735cf3ad2bea56d180759738b7823028de2
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-09-30 19:32:43 -0700

    add styled components for title and text

---
+++ src/components/ResearchCard/ResearchCard.elements.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CardTitle = styled.ul`
+  font-weight: bolder;
+  font-size: 35px;
+  text-align: center;
+`;
+
+export const CardText = styled.li`
+  margin: 15px 15px;
+`;
