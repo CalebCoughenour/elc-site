8d79eb69fa3284145bf8b24b595348966664d858
Caleb Coughenour <calebcoughenour@protonmail.com>
2022-10-08 19:39:28 -0700

    add card and container for mission sections

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import  { missionObjOne, missionObjTwo, missionObjThree, missionObjFour, bannerObj } from './Data';
 import { BannerSection, MissionSection, MissionSectionRight, HomeVideoSection } from '../../components';
-
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 
 const Home = () => {
   return (
     <>
       <BannerSection {...bannerObj} />
       <div style={{ background: '#383232' }} >
-        <MissionSection {...missionObjOne} />
-        <MissionSectionRight {...missionObjTwo} />
-        <MissionSection {...missionObjThree} />
-        <MissionSectionRight {...missionObjFour} />
+        <Container>
+          <Card style={{ border: 'none', background: '#383232' }}>
+            <MissionSection {...missionObjOne} />
+            <MissionSectionRight {...missionObjTwo} />
+            <MissionSection {...missionObjThree} />
+            <MissionSectionRight {...missionObjFour} />
+          </Card>
+        </Container>
+        
       </div>
       <HomeVideoSection />
     </>
